4e5a00d777bef266e7ccf18e4f8d06bd1acd3441
morteza <morteza.kia8700@gmail.com>
2024-01-08 17:40:56 +0330

    change the axis values

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState } from "react"
 import modifyPdf from "./PdfModifier"
 
-
-const copyArrayBuffer = (arrayBuffer) => {
-  const copiedArrayBuffer = new ArrayBuffer(arrayBuffer.byteLength);
-  new Uint8Array(copiedArrayBuffer).set(new Uint8Array(arrayBuffer));
-  return copiedArrayBuffer;
-};
-
-
-
 function App() {
-  const [val,setVal] = useState()
+  const [state,setState] = useState({
+    axisX:'',
+    axisY:'',    
+    borderWidth:''
+  })
 
   const change = async (e)=>{
-    let x = e.target.files[0]
-    let xx = await x.arrayBuffer()
-    modifyPdf(xx);
+    let file = e.target.files[0]
+    let arrBuffer = await file.arrayBuffer()
+    modifyPdf(arrBuffer , state);
   }
 
-  useEffect(()=>{
-    if(val){
-      // let x = copyArrayBuffer(val)
-      // console.log(x);
-    }
-  },[val])
-
   return (
-    <> 
-      <input type="file" value={val} onChange={e => change(e)}/>
-    </>
+    <div className="d-flex flex-column gap-10p"> 
+      <h3>Description : Accepts Negative And Positive Values Along The Cordinate Axis (x and y)</h3>
+      <div className="d-flex gap-10p">
+        <label htmlFor="X">axisX :</label>
+        <input type="number" id="X" name="axisX" value={state.axisX} onChange={e => setState({...state ,[e.target.name]:+e.target.value})} />
+      </div>
+      <div className="d-flex gap-10p">
+        <label htmlFor="Y">axisY :</label>
+        <input type="number" id="Y" name="axisY" value={state.axisY} onChange={e => setState({...state ,[e.target.name]:+e.target.value})} />
+      </div>
+      <div className="d-flex gap-10p">
+        <label htmlFor="">borderWidth : </label>
+        <input type="number" name="borderWidth" value={state.borderWidth} onChange={e => setState({...state ,[e.target.name]:+e.target.value})} />
+      </div>
+      <input type="file" onChange={e => change(e)}/>
+    </div>
   )
 }
 
